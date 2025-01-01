5a980fee4492b25cd541faae77e07bf95007459b
hoyoung1359 <hoyoung1359@yonsei.ac.kr>
2025-01-02 02:54:42 +0900

    Add vercel.json configuration

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Wk2DZJeRN0NkTbr6uY4CoSjsRs7/VbUhAgRNPKWec6k=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"pFr0rrV/CD4ifOD9F1AD5rN+Yf/TA/DpDZpvs/KeZDE=\"}"
